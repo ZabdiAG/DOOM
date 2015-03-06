b13b20d7508357960f887fd0807c60e219ee97b2
Ernesto Angel Celis de la Fuente <developer@celisdelafuente.net>
2015-03-06 01:08:34 -0600

    WIP google sample auto SN accs

--- bots/johnny5.sh
+++ bots/johnny5.sh
@@ -1,42 +1,54 @@
 #!/bin/env bash
 
-URL="$1"
 TEMPDIR=/tmp
 WORKDIR="$TEMPDIR/goose/$(date +%Y%m%d%H%M%s)"
 CWD=$(pwd)
 CMD="/usr/bin/curl"
-CGET="$URL -o $WORKDIR/raw.html"
+RAWFILE="raw.html"
+
+## Utility functions
+## Look for Form fileds
+function lookFields {
+  grep '<input.*>' $WORKDIR/$RAWFILE
+}
 
 ## VARIABLE="FieldName" <-- The name attribute of the form field
-# Google
-FNAME="FirstName"
-LNAME="LastName"
-UNAME="GmailAddress"
-PASSWD="Passwd"
-PASSWDC="PasswdAgain"
-LOCALE="es"
-INPUT="Input" # BirthDay
-BMONTH="BirthMonth" # 10
-BDAY="BirthDay" # 12
-BYEAR="BirthYear" # 1978
-GENDER="Gender" # FEMALE MALE OTHER
-EMAIL="RecoveryEmailAddress"
-TOS="TermsOfService" # yes
-# VALUES
-FNAME="FirstName"
-LNAME="LastName"
-UNAME="GmailAddress"
-PASSWD="Passwd"
-PASSWDC="PasswdAgain"
-LOCALE="es"
-INPUT="Input" # BirthDay
-BMONTH="BirthMonth" # 10
-BDAY="BirthDay" # 12
-BYEAR="BirthYear" # 1978
-GENDER="Gender" # FEMALE MALE OTHER
-EMAIL="RecoveryEmailAddress"
-TOS="TermsOfService" # yes
+function google {
+  URL="https://accounts.google.com/SignUp"
+  CGET="$URL -o $WORKDIR/$RAWFILE"
+  ## Form fields
+  FNAME="FirstName"
+  LNAME="LastName"
+  UNAME="GmailAddress"
+  PASSWD="Passwd"
+  PASSWDC="PasswdAgain"
+  LOCALE="es"
+  INPUT="Input" # BirthDay
+  BMONTH="BirthMonth" # 10
+  BDAY="BirthDay" # 12
+  BYEAR="BirthYear" # 1978
+  GENDER="Gender" # FEMALE MALE OTHER
+  EMAIL="RecoveryEmailAddress"
+  TOS="TermsOfService" # yes
+  # Form VALUES
+  FNAME="FirstName"
+  LNAME="LastName"
+  UNAME="GmailAddress"
+  PASSWD="Passwd"
+  PASSWDC="PasswdAgain"
+  LOCALE="es"
+  INPUT="Input" # BirthDay
+  BMONTH="BirthMonth" # 10
+  BDAY="BirthDay" # 12
+  BYEAR="BirthYear" # 1978
+  GENDER="Gender" # FEMALE MALE OTHER
+  EMAIL="RecoveryEmailAddress"
+  TOS="TermsOfService" # yes
+  ## Hidden fields
+  TIMESTMP="timeStmp"
+  SECTOK="secTok"
 
+}
 
 
 if [ ! -x $WORKDIR ] ; then
@@ -45,6 +57,14 @@ if [ ! -x $WORKDIR ] ; then
 fi
 cd $WORKDIR
 
+## Which service?
+case "$1" in
+  google)   google
+    ;;
+  *)
+    echo "Invalid!"
+    ;;
+esac
 ## multipart/form-data
 POSTDATA="\"$FNAME=$VFNAME;$LNAME=$VLNAME;$UNAME=$VUNAME"
 POSTDATA="$POSTDATA;$PASSWD=$VPASSWD;$PASSWDC=$VPASSWDC"
@@ -54,8 +74,8 @@ POSTDATA="$POSTDATA;$BMONTH=$VBMONTH;$BDAY=$VBDAY"
 POSTDATA="$POSTDATA;$BYEAR=$VBYEAR;$GENDER=$VGENDER"
 POSTDATA="$POSTDATA;$EMAIL=$VEMAIL;$TOS=$VTOS\""
 
-## 1. Initial GET request
-echo $CMD $CGET
-echo
-echo $CMD -XPOST -F$POSTDATA
 
+## Process
+$CMD $CGET
+echo $CMD -XPOST -F$POSTDATA
+lookFields
