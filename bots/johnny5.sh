7b00d62769c9d395f71b6d09fd8071a8d5f19e63
ZabdiAG <aguilera.glz.luis@gmail.com>
2015-03-06 18:14:02 -0600

    Facebook part

--- bots/johnny5.sh
+++ bots/johnny5.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 TEMPDIR=/tmp
 WORKDIR="$TEMPDIR/goose/$(date +%Y%m%d%H%M%s)"
 CWD=$(pwd)
@@ -43,6 +44,62 @@ function google {
   GENDER="Gender" # FEMALE MALE OTHER
   EMAIL="RecoveryEmailAddress"
   TOS="TermsOfService" # yes
+  # Form VALUES
+  VFNAME="FirstName"
+  VLNAME="LastName"
+  VUNAME="GmailAddress"
+  VPASSWD="Passwd"
+  VPASSWDC="PasswdAgain"
+  VLOCALE="es"
+  VINPUT="BirthDay"
+  VBMONTH="10"
+  VBDAY="12"
+  VBYEAR="1978"
+  VGENDER="OTHER" # FEMALE MALE OTHER
+  VEMAIL="$OURINBOX"
+  VTOS="yes" # yes
+  ## Hidden fields
+  VTIMESTMP="timeStmp"
+  VSECTOK="secTok"
+  ## Get registration form
+  $CMD
+  ## Process
+  lookFields
+}
+function facebook{
+  mkWorkdir
+  URL="https://facebook.com"
+  CMD="$CMD $URL -o $WORKDIR/$RAWFILE"
+  ## Form fields
+  FNAME="firstname"
+  LNAME="lastname"
+  UNAME="reg_email__"
+  UNAMECONF="reg_email_confirmation__"
+  PASSWD="reg_passwd__"
+#  PASSWDC="PasswdAgain"
+  LOCALE="locale"
+  INPUT="Input" # BirthDay
+  BMONTH="birthday_month" # 10
+  BDAY="birthday_day" # 12
+  BYEAR="birthday_year" # 1978
+  GENDER="sex" # FEMALE MALE OTHER
+#  EMAIL="RecoveryEmailAddress"
+#  TOS="TermsOfService" # yes
+  WEBSUBMIT="websubmit"
+  referrer="referrer"
+  asked_to_login="asked_to_login"
+  terms="terms"
+  ab_test_data="ab_test_data"
+  contactpoint_label="contactpoint_label"
+  locale="locale"
+  reg_instance="reg_instance"
+  captcha_persist_data="captcha_persist_data"
+  captcha_session="captcha_session"
+  extra_challenge_params="extra_challenge_params"
+  recaptcha_type="recaptcha_type"
+  captcha_response="captcha_response"
+  qsstamp="qsstamp"
+
   # Form VALUES
   VFNAME="FirstName"
   VLNAME="LastName"
